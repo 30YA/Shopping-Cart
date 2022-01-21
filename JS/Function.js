680474974bc69b2019de9d2e4e683795948eccda
siavash aghazadeh <siavash.aghazadeh@gmail.com>
2022-01-21 15:37:05 +0330

    Start OOP

--- JS/Function.js
+++ JS/Function.js
@@ -1,80 +1,45 @@
 "use strict";
-//------------------------------------
-const products = [
-    {
-        id: 1,
-        name: 'Sony Alpha',
-        description: 'Mirrorless Interchangeable Lens',
-        price: 24000,
-        off:'30 %',
-        like: 58,
-    },
-    {
-        id: 2,
-        name: 'Sony 6400',
-        description: 'Mirrorless Interchangeable Lens',
-        price: 15000,
-        off:'80 %',
-        like: 148,
-    },
-    {
-        id: 3,
-        name: 'Canon D90',
-        description: 'Mirrorless Interchangeable Lens',
-        price: 18000,
-        off:'10 %',
-        like: 596,
-    },
-    {
-        id: 4,
-        name: 'Nikon D800',
-        description: 'Mirrorless Interchangeable Lens',
-        price: 35000,
-        off:'15 %',
-        like: 110,
-    },
-    {
-        id: 5,
-        name: 'Canon D850',
-        description: 'Mirrorless Interchangeable Lens',
-        price: 22000,
-        off:'25 %',
-        like: 363,
-    },
-    {
-        id: 5,
-        name: 'Sony T8',
-        description: 'Mirrorless Interchangeable Lens',
-        price: 9000,
-        off:'45 %',
-        like: 263,
+// ----------------------------------
+// get products :
+class Products{
+    getProducts(){
+        return Allproducts;
     }
-]
-// this function add all products to Html and show it to user
-function loadProducts(products,addtoMain) {
-    const sia = [];
-    products.forEach(item => {
-        sia.push(`
-        <div class="product">
-          <div class="title">
-            <span><i class="fas fa-heart"></i> ${item.like}</span>
-            <span>${item.off} off</span>
-          </div>
-          <div class="info">
-            <img src="img/1.png" alt="">
-            <h3>${item.name}</h3>
-            <h6>${item.description}</h6>
-            <button>add to Cart</button>
-          </div>
-        </div>`
-        );
-    });
-    const div = document.createElement('div');
-    div.classList = 'products';
-    for(let key of sia){
-        div.innerHTML += key;
+}
+// display products :
+class UI {
+    displayProducts(products){
+        const list = [];
+        products.forEach(item => {
+            list.push(`
+            <div class="product">
+              <div class="title">
+                <span><i class="fas fa-heart"></i> ${item.like}</span>
+                <span>${item.off} off</span>
+              </div>
+              <div class="info">
+                <img src=${item.imgURL} alt="">
+                <h3>${item.name}</h3>
+                <h6>${item.description}</h6>
+                <button class="addToCart" data-id="${item.id}">add to Cart</button>
+              </div>
+            </div>`
+            );
+        });
+        const div = document.createElement('div');
+        div.classList = 'products';
+        for(let key of list){
+            div.innerHTML += key;
+        }
+        addtoMain.append(div);
     }
-    addtoMain.append(div);
+    getAddToCartBTN(){
+        const nodelist = document.querySelectorAll('.addToCart');
+        const addToCart = [...nodelist];
+        addToCart.forEach(btn => {
+            const id = btn.dataset.id
+        })
+    }
 }
 //basket click : -------------------------------
 function openModal() {
--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,71 @@
 "use strict";
+const Allproducts = [
+    {
+        id: 1,
+        name: 'Sony Alpha',
+        description: 'Mirrorless Interchangeable Lens',
+        price: 24000,
+        imgURL: 'img/1.png',
+        off:'30 %',
+        like: 58,
+    },
+    {
+        id: 2,
+        name: 'Sony 6400',
+        description: 'Mirrorless Interchangeable Lens',
+        price: 15000,
+        imgURL: 'img/2.png',
+        off:'80 %',
+        like: 148,
+    },
+    {
+        id: 3,
+        name: 'Canon D90',
+        description: 'Mirrorless Interchangeable Lens',
+        price: 18000,
+        imgURL: 'img/3.png',
+        off:'10 %',
+        like: 596,
+    },
+    {
+        id: 4,
+        name: 'Nikon D800',
+        description: 'Mirrorless Interchangeable Lens',
+        price: 35000,
+        imgURL: 'img/4.png',
+        off:'15 %',
+        like: 110,
+    },
+    {
+        id: 5,
+        name: 'Canon D850',
+        description: 'Mirrorless Interchangeable Lens',
+        price: 22000,
+        imgURL: 'img/5.png',
+        off:'25 %',
+        like: 363,
+    },
+    {
+        id: 6,
+        name: 'Sony T8',
+        description: 'Mirrorless Interchangeable Lens',
+        price: 9000,
+        imgURL: 'img/1.png',
+        off:'45 %',
+        like: 263,
+    }
+]
 // -----------------------------------------------------
 const addtoMain = document.querySelector('.main');
-loadProducts(products,addtoMain);
-
+document.addEventListener('DOMContentLoaded', () => {
+    const products = new Products();
+    const ui = new UI();
+    ui.displayProducts(products.getProducts());
+    ui.getAddToCartBTN();
+})
 //basket click : -------------------------------
 const basket = document.querySelector('.basket');
-basket.addEventListener('click', openModal);
-
 const close = document.querySelector('.close');
+basket.addEventListener('click', openModal);
 close.addEventListener('click', closeModal);
+
